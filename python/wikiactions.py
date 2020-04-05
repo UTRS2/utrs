b4b862e08649491881bca407fe76005073c4eec6
Amanda <deltaquadwiki@gmail.com>
2020-04-05 19:18:57 -0400

    convert

--- python/wikiactions.py
+++ python/wikiactions.py
@@ -84,9 +84,9 @@ UTRS Developers"""
             calldb("delete from wikitasks where id="+str(wtid)+";","write")
             checkPerms(username,user)
 def checkPerms(user, id):
-    enperms = array("user"=False,"sysop"=False,"checkuser"=False,"oversight"=False)
-    ptperms = array("user"=False,"sysop"=False,"checkuser"=False,"oversight"=False)
-    metaperms = array("user"=False,"steward"=False,"staff"=False)
+    enperms = {"user":False,"sysop":False,"checkuser":False,"oversight":False}
+    ptperms = {"user":False,"sysop":False,"checkuser":False,"oversight":False}
+    metaperms = {"user":False,"steward":False,"staff":False}
     ##############################
     ###Enwiki checks##############
     params = {'action': 'query',
