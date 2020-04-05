e202e1c11479c5b3604b7fa48c55ad7fccb7c23a
Amanda <deltaquadwiki@gmail.com>
2020-04-04 21:12:08 -0400

    Debug

--- python/wikiactions.py
+++ python/wikiactions.py
@@ -90,6 +90,7 @@ def checkPerms(user):
             'usprop': 'groups'
             }
     raw = callAPI(params)
+    print raw["query"]
     result = raw["query"]["users"]["groups"]
     print result
 #verifyusers()
