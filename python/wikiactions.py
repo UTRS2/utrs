eee957a2f75648486519a695dd88ebb3b7745d4e
Amanda <deltaquadwiki@gmail.com>
2020-04-29 13:35:44 -0400

    add url

--- python/wikiactions.py
+++ python/wikiactions.py
@@ -324,7 +324,7 @@ def appeallist():
     fulltext+=top
     results = calldb("select * from appeals where status != 'CLOSED' AND status !='VERIFY' AND status != 'NOTFOUND';","read")
     for result in results:
-        fulltext += "\n|-\n|"+str(result[0])+"\n|"+str(result[1])+"\n|"+str(result[9])+"\n|"+str(result[5])
+        fulltext += "\n|-\n|[https://utrs-beta.wmflabs.org/appeal/"+str(result[0])+" "+str(result[0])+"]\n|"+str(result[1])+"\n|"+str(result[9])+"\n|"+str(result[5])
     fulltext +="\n|}"
     page = masterwiki.pages["User:DeltaQuad/UTRS Appeals"]
     page.save(fulltext, "Updating UTRS caselist")
