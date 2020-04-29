dbd1b5acc463fa9cb4549140773be473eca40736
Amanda <deltaquadwiki@gmail.com>
2020-04-29 13:41:29 -0400

    blocks get autoverified

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -188,7 +188,6 @@ class AppealController extends Controller
         $appeal = Appeal::create($input);
         $cudata = Privatedata::create(array('appealID' => $appeal->id,'ipaddress' => $ip, 'useragent' => $ua, 'language' => $lang));
         $log = Log::create(array('user' => 0, 'referenceobject'=>$appeal['id'],'objecttype'=>'appeal','action'=>'create','ip' => $ip, 'ua' => $ua . " " .$lang));
-        Wikitask::create(['task'=>'verifyblock','actionid'=>$appeal->id]);
         return view ('appeals.makeappeal.hash', ['hash'=>$key]);
     }
     public function ipappeal() {
