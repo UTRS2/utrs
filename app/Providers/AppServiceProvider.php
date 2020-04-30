80771f51fcb03d65d5dd1ebe18ee4d896b7dc119
Amanda <deltaquadwiki@gmail.com>
2020-04-30 10:23:11 -0400

    try again

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
