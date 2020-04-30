9d08f8a1d466bc11fda9805545d70790faaf684a
Amanda <deltaquadwiki@gmail.com>
2020-04-30 10:27:26 -0400

    force regardless

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,8 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (env('APP_ENV') == 'production') {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
