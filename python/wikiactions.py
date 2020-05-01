ae71cd92c1b7f89ccf8682816d2d602b050f769b
Amanda <deltaquadwiki@gmail.com>
2020-05-01 17:27:53 -0400

    what kind of backwards logic was that

--- python/wikiactions.py
+++ python/wikiactions.py
@@ -329,7 +329,7 @@ def appeallist():
     !Status
     """
     fulltext+=top
-    results = calldb("select * from appeals where status != 'CLOSED' AND status !='VERIFY' AND status != 'NOTFOUND' AND status != 'EXPIRED' AND status != 'DECLINE' AND status != 'ACCEPT';","read")
+    results = calldb("select * from appeals where status != 'CLOSED' AND status !='VERIFY' AND status != 'NOTFOUND' AND status != 'EXPIRED' AND status != 'DECLINE' AND status != 'ACCEPT' AND status != 'INVALID';","read")
     for result in results:
         fulltext += "\n|-\n|[https://utrs-beta.wmflabs.org/appeal/"+str(result[0])+" "+str(result[0])+"]\n|"+str(result[1])+"\n|"+str(result[9])+"\n|"+str(result[5])
     fulltext +="\n|}"
