e6b488bf9521b36f20c7556056b22b2eb893c84c
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2020-05-03 02:14:19 -0400

    ASCII errors

--- python/wikiactions.py
+++ python/wikiactions.py
@@ -346,7 +346,7 @@ def appeallist():
     fulltext+=top
     results = calldb("select * from appeals where status != 'CLOSED' AND status !='VERIFY' AND status != 'NOTFOUND' AND status != 'EXPIRED' AND status != 'DECLINE' AND status != 'ACCEPT' AND status != 'INVALID';","read")
     for result in results:
-        fulltext += "\n|-\n|[https://utrs-beta.wmflabs.org/appeal/"+str(result[0])+" "+str(result[0])+"]\n|"+str(result[1])+"\n|"+str(result[9])+"\n|"+str(result[5])
+        fulltext += "\n|-\n|[https://utrs-beta.wmflabs.org/appeal/"+str(result[0])+" "+str(result[0])+"]\n|"+result[1].encode('utf-8').strip()+"\n|"+str(result[9])+"\n|"+str(result[5])
     fulltext +="\n|}"
     page = masterwiki.pages["User:DeltaQuad/UTRS Appeals"]
     page.save(fulltext, "Updating UTRS caselist")
