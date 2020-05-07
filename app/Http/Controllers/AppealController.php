0397fa173c93e13273424234a9275468020ed2b7
Amanda <deltaquadwiki@gmail.com>
2020-05-07 00:39:36 -0400

    proper redirect

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -436,7 +436,7 @@ class AppealController extends Controller
             $appeal->status = strtoupper($type);
             $appeal->save();
             $log = Log::create(array('user' => $user, 'referenceobject'=>$id,'objecttype'=>'appeal','action'=>'closed - '.$type,'ip' => $ip, 'ua' => $ua . " " .$lang, 'protected'=>0));
-            return redirect('appeal/'.$id);
+            return redirect('/review');
         }
         else {
             abort(403);
@@ -458,7 +458,7 @@ class AppealController extends Controller
             $appeal->privacyreview = 2;
             $appeal->save();
             $log = Log::create(array('user' => $user, 'referenceobject'=>$id,'objecttype'=>'appeal','action'=>'sent for privacy review','ip' => $ip, 'ua' => $ua . " " .$lang, 'protected'=>0));
-            return redirect('appeal/'.$id);
+            return redirect('/review');
         }
         else {
             abort(403);
