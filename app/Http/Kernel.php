8fb88700112fcb517d09a3ea4af0c58c5d6e2a6c
Taavi Väänänen <hi@tassu.me>
2020-05-10 10:17:59 +0300

    Redirect to HTTPS when APP_ENV=production (closes #28)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ForceHttps;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -19,6 +20,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+
+        ForceHttps::class,
     ];
 
     /**
