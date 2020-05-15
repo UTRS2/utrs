be7868aac7570c4207914bb43d9d27de894bbbaa
dqwiki <deltaquadwiki@gmail.com>
2020-05-15 18:59:02 -0400

    Add message

--- config/app.php
+++ config/app.php
@@ -113,9 +113,9 @@ return [
     | Use XFF
     |--------------------------------------------------------------------------
     |
-    | This locale will be used by the Faker PHP library when generating fake
-    | data for your database seeds. For example, this will be used to get
-    | localized telephone numbers, street address information and more.
+    | Use XFF IP instead of the regular server IP.
+    | 
+    | 
     |
     */
 
