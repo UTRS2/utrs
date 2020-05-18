eb10bf3a3a9369d5df9a6b74b4d37202aa0cb67f
Taavi Väänänen <hi@tassu.me>
2020-05-18 09:52:23 +0300

    All the formatting changes (#102)

--- resources/views/appeals/unverifiedappeal.blade.php
+++ resources/views/appeals/unverifiedappeal.blade.php
@@ -2,16 +2,17 @@
 @section('title', 'Appeal not verified')
 @section('content')
 
-	<br />
-  <center>
-  <div class="card border-danger mb-3" style="max-width: 18rem;">
-    <div class="card-header">Appeal not verified</div>
-      <div class="card-body text-danger">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Commons-emblem-hand.svg/48px-Commons-emblem-hand.svg.png" width="96px">
-        <p class="card-text">This appeal has not been verified. Only Developers can see it.</p>
-        <p class="card-text"><a href="/">Go Home</a></p>
-      </div>
-    </div>
-  </div>
-  </center>
+    <br/>
+    <center>
+        <div class="card border-danger mb-3" style="max-width: 18rem;">
+            <div class="card-header">Appeal not verified</div>
+            <div class="card-body text-danger">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/16/Commons-emblem-hand.svg/48px-Commons-emblem-hand.svg.png"
+                     width="96px">
+                <p class="card-text">This appeal has not been verified. Only Developers can see it.</p>
+                <p class="card-text"><a href="/">Go Home</a></p>
+            </div>
+        </div>
+        </div>
+    </center>
 @endsection
