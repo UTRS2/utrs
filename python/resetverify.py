99eaa25b85d6e9136c3634f4bb3da8047b1fd691
dqwiki <deltaquadwiki@gmail.com>
2020-05-17 21:30:34 -0400

    add reset script

---
+++ python/resetverify.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import mysql.connector
+from mysql.connector import Error
+import credentials
+
+def calldb(command,style):
+    try:
+        print command
+        connection = mysql.connector.connect(host=credentials.ip,
+                                             database=credentials.database,
+                                             user=credentials.user,
+                                             password=credentials.password)
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute(command)
+            if style == "read":
+                record = cursor.fetchall()
+            if style == "write":
+                connection.commit()
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+        if style == "read":return record
+        else:return "Done"
+
+results = calldb("select id from users;","read")
+for result in result:
+	results = calldb("insert into wikitasks (task,actionid) VALUES ('verifyaccount',"+str(id)+");","write")
+	print "RESET VERIFYACCOUNT ON ID: "+result[0]
