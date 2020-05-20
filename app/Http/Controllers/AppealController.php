1a116fdf192d5f113fb3468d91d97285605ddfcc
dqwiki <deltaquadwiki@gmail.com>
2020-05-20 19:59:48 -0400

    re-add function

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -251,8 +251,8 @@ class AppealController extends Controller
         $banacct = Ban::where('ip','=',0)->get();
         $banip = Ban::where('ip','=',1)->get();
         foreach ($banip as $ban) {
-            if (ip_in_range($ban->target)) {
-                return view('appeals.ban', ['expire'=>$ban->expiry],'id'=>$ban->id]);
+            if (self::ip_in_range($ip,$ban->target)) {
+                return view('appeals.ban', ['expire'=>$ban->expiry,'id'=>$ban->id]);
             }
         }
 
@@ -645,5 +645,24 @@ class AppealController extends Controller
         ]);
 
         return redirect()->to('/publicappeal?hash=' . $appeal->appealsecretkey);
+    }
+
+    /**
+     * Check if a given ip is in a network
+     * @param  string $ip    IP to check in IPV4 format eg. 127.0.0.1
+     * @param  string $range IP/CIDR netmask eg. 127.0.0.0/24, also 127.0.0.1 is accepted and /32 assumed
+     * @return boolean true if the ip is in this range / false if not.
+     */
+    public static function ip_in_range( $ip, $range ) {
+        if ( strpos( $range, '/' ) == false ) {
+            $range .= '/32';
+        }
+        // $range is in IP/CIDR format eg 127.0.0.1/24
+        list( $range, $netmask ) = explode( '/', $range, 2 );
+        $range_decimal = ip2long( $range );
+        $ip_decimal = ip2long( $ip );
+        $wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
+        $netmask_decimal = ~ $wildcard_decimal;
+        return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
     }
 }
