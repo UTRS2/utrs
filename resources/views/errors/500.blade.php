0cc11a32c677d4c233d61c68f3ef074c4e7356e0
Taavi Väänänen <hi@tassu.me>
2020-05-21 10:26:42 +0300

    Create custom 500 page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Server error &ndash; {{ config('app.name') }}</title>
+    <link rel="stylesheet" href="https://tools-static.wmflabs.org/cdnjs/ajax/libs/twitter-bootstrap/4.4.0/css/bootstrap.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+</head>
+<body>
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <a class="navbar-brand" href="/">{{ config('app.name') }}</a>
+</nav>
+
+<div class="container mt-4">
+    <div class="alert alert-danger" role="alert">
+        <h4 class="alert-heading"><b>Uh-oh.</b> UTRS has suffered an internal exception and can't continue to serve your request.</h4>
+
+        <p>We apologise for any inconvenience caused. Please report this exception at <a href="https://github.com/utrs2/utrs/issues" class="alert-link">https://github.com/utrs2/utrs/issues</a>.</p>
+        <p>If this happened while creating your appeal, do not create another appeal as it might have been saved. Instead, contact UTRS developers using the link above and mention this so they can help you.</p>
+    </div>
+</div>
+</body>
+</html>
