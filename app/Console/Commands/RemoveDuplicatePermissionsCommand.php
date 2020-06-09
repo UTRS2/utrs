141a2efdcc05e59e3124c161d4b9e245179d8ec7
Taavi Väänänen <hi@tassu.me>
2020-06-09 08:26:21 +0300

    Cleanup dry run handling

--- app/Console/Commands/RemoveDuplicatePermissionsCommand.php
+++ app/Console/Commands/RemoveDuplicatePermissionsCommand.php
@@ -8,7 +8,7 @@ use Illuminate\Console\Command;
 class RemoveDuplicatePermissionsCommand extends Command
 {
     protected $signature = 'utrs-maintenance:remove-duplicate-permissions 
-                            {--dry? : A dry run of the command should be ran}';
+                            {--dry : A dry run of the command should be ran}';
     protected $description = 'Clears up legacy duplicate permissions from the database';
 
     public function __construct()
@@ -18,34 +18,40 @@ class RemoveDuplicatePermissionsCommand extends Command
 
     public function handle()
     {
-        if ($this->argument('--dry')!==NULL) {
-            if (!$this->confirm('You are about to run a live database change. Are you sure? (Did you test it?)')) {
-                return 1;
-            }
+        $dry = $this->option('dry');
+
+        if (!$dry && !$this->confirm('You are about to run a live database change. Are you sure? (Did you test it?)')) {
+            $this->warn('Operation aborted.');
+            return 1;
         }
+
         $duplicates = Permission::with('user')
             ->selectRaw('wiki, userid, count(*) as count')
             ->groupBy(['wiki', 'userid'])
             ->having('count', '>', 1)
             ->get();
 
+        if ($duplicates->isEmpty()) {
+            $this->warn('No duplicates found.');
+            return 1;
+        }
+
         $progressBar = $this->output->createProgressBar($duplicates->count());
-        $progressBar->display();
+        $progressBar->start();
 
         foreach ($duplicates as $duplicate) {
-            if ($this->argument('--dry')!==NULL) {
-                /** @var Permission $duplicate */
-                $this->info("Removing duplicates for user $duplicate->userid in wiki $duplicate->wiki...");
+            /** @var Permission $duplicate */
+
+            if ($dry) {
+                $this->info("DRY: Would remove $duplicates->count duplicates for user $duplicate->userid in wiki $duplicate->wiki");
+            } else {
+                $this->info("Removing $duplicates->count duplicates for user $duplicate->userid in wiki $duplicate->wiki...");
 
                 // delete duplicates. can't use $duplicate->delete() due to it being a groupBy
                 Permission::where('userid', $duplicate->userid)->where('wiki', $duplicate->wiki)->delete();
 
                 // and load correct permissions from wiki
                 $duplicate->user->queuePermissionChecks();
-            }
-            else {
-                /** @var Permission $duplicate */
-                $this->info("DRY: Wanting to removing duplicates for user $duplicate->userid in wiki $duplicate->wiki");
             }
 
             $progressBar->advance();
