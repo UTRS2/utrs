b06f800916798a3255412b3e825e9867d5a1f3eb
Taavi Väänänen <hi@tassu.me>
2020-07-04 13:35:13 +0300

    Cleanup appeal sorting, improve appeal list styling

--- resources/views/appeals/appeallist.blade.php
+++ resources/views/appeals/appeallist.blade.php
@@ -46,61 +46,75 @@
         </div>
     </div>
     @foreach($appealtypes as $type)
-    <div class="card mt-4">
-        <h5 class="card-header">{{ $type }}</h5>
-        <div class="card-body">
-            <table class="table table-bordered table-dark">
-                <thead>
-                <tr>
-                    <th scope="col">ID #</th>
-                    <th scope="col">Subject</th>
-                    <th scope="col">Status/Type/Wiki</th>
-                    <th scope="col">Blocking Admin</th>
-                    <th scope="col">Block Reason</th>
-                    <th scope="col">Date</th>
-                </tr>
-                </thead>
-                <tbody>
+        <div class="card mt-4">
+            <h5 class="card-header">{{ $type }}</h5>
+            <div class="card-body">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">ID #</th>
+                        <th scope="col">Subject</th>
+                        <th scope="col">Status/Type/Wiki</th>
+                        <th scope="col">Blocking Admin</th>
+                        <th scope="col">Block Reason</th>
+                        <th scope="col">Date</th>
+                    </tr>
+                    </thead>
+                    <tbody>
                     @foreach($appeals[$type] as $appeal)
-                        @if($appeal->status === "ADMIN")
-                            <tr class="bg-primary">
-                        @elseif($appeal->status === "CHECKUSER")
-                            <tr class="bg-warning" style="color: #212529!important;">
+                        @if($appeal->status === \App\Appeal::STATUS_ADMIN)
+                            <tr class="table-primary">
+                        @elseif($appeal->status === \App\Appeal::STATUS_CHECKUSER)
+                            <tr class="table-warning">
                         @else
                             <tr>
-                        @endif
-                            <td style="vertical-align: middle;">
-                                @isset($appeal['handlingadmin'])
-                                    <a href="/appeal/{{ $appeal['id'] }}" class="btn btn-danger">
-                                @else
-                                    <a href="/appeal/{{ $appeal['id'] }}" class="btn btn-primary">
-                                @endisset
-                                    #{{ $appeal->id }}
-                                </a>
-                            </td>
-                            <td style="vertical-align: middle;">{{ $appeal['appealfor'] }}</td>
-                            <td style="vertical-align: middle">
-                                {{ $appeal->status }}<br/>
-                                @if($appeal->blocktype === 0)
-                                    IP address
-                                @elseif($appeal->blocktype === 1)
-                                    Account
-                                @elseif($appeal->blocktype === 2)
-                                    IP underneath account
-                                @else
-                                    Unknown type: {{ $appeal->blocktype }}
                                 @endif
-                                on {{ $appeal->wiki }}
-                            </td>
-                            <td style="vertical-align: middle;">{{ $appeal['blockingadmin'] }}</td>
-                            <td style="vertical-align: middle;">{!! $appeal->getFormattedBlockReason('style="color: #00ffea!important;"') !!}</td>
-                            <td style="vertical-align: middle;">{{ $appeal['submitted'] }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                <td style="vertical-align: middle;">
+                                    @isset($appeal->handlingadmin)
+                                        @if(Auth::check() && $appeal->handlingadmin == Auth::id())
+                                            <a href="{{ route('appeal.view', $appeal) }}" class="btn btn-info">
+                                                @else
+                                                    <a href="{{ route('appeal.view', $appeal) }}" class="btn btn-danger">
+                                                        @endif
+                                                        @else
+                                                            <a href="{{ route('appeal.view', $appeal) }}" class="btn btn-primary">
+                                                                @endisset
+                                                                #{{ $appeal->id }}
+                                                            </a>
+                                </td>
+                                <td style="vertical-align: middle;">{{ $appeal->appealfor }}</td>
+                                <td style="vertical-align: middle">
+                                    {{ $appeal->status }}<br/>
+                                    @if($appeal->blocktype === 0)
+                                        IP address
+                                    @elseif($appeal->blocktype === 1)
+                                        Account
+                                    @elseif($appeal->blocktype === 2)
+                                        IP underneath account
+                                    @else
+                                        Unknown type: {{ $appeal->blocktype }}
+                                    @endif
+                                    on {{ $appeal->wiki }}<br/>
+                                    @if($appeal->handlingAdminObject)
+                                        Reserved by
+                                        @can('view', $appeal->handlingAdminObject)
+                                            <a href="{{ route('admin.users.view', $appeal->handlingAdminObject) }}">
+                                                {{ $appeal->handlingAdminObject->username }}
+                                            </a>
+                                        @else
+                                            {{ $appeal->handlingAdminObject->username }}
+                                        @endcan
+                                    @endif
+                                </td>
+                                <td style="vertical-align: middle;">{{ $appeal->blockingadmin }}</td>
+                                <td style="vertical-align: middle;">{!! $appeal->getFormattedBlockReason() !!}</td>
+                                <td style="vertical-align: middle;">{{ $appeal->submitted }}</td>
+                            </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
     @endforeach
 
 @endsection
