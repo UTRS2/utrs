0aca5a42bc1624ea17a45678e60679a1041c220c
Taavi Väänänen <hi@tassu.me>
2020-09-10 20:51:52 +0300

    Manually apply namespace to routes, comments on variable seem wrong

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,10 +30,12 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
             Route::prefix('api')
                 ->middleware('api')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
         });
     }
