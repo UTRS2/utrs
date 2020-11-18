7344826c7112d1581a5b80f63814d05f4b44aabe
Taavi Väänänen <hi@tassu.me>
2020-11-18 17:39:04 +0200

    Use a cookie jar for MediaWiki session

--- app/Services/MediaWiki/Implementation/RealMediaWikiApi.php
+++ app/Services/MediaWiki/Implementation/RealMediaWikiApi.php
@@ -2,6 +2,11 @@
 
 namespace App\Services\MediaWiki\Implementation;
 
+use Throwable;
+use GuzzleHttp\Client;
+use GuzzleHttp\Cookie\CookieJar;
+use Mediawiki\Api\SimpleRequest;
+use GuzzleHttp\Cookie\FileCookieJar;
 use App\Services\MediaWiki\Api\MediaWikiApi;
 use App\Services\MediaWiki\Api\MediaWikiExtras;
 use Mediawiki\Api\ApiUser;
@@ -17,9 +22,36 @@ class RealMediaWikiApi implements MediaWikiApi
     /** @var AddwikiMediaWikiApi */
     private $api;
 
-    public function __construct(string $url)
+    /** @var Client */
+    private $guzzleClient;
+
+    /** @var boolean */
+    private $hasExistingSession = false;
+
+    public function __construct(string $identifier, string $url)
     {
-        $this->api = new AddwikiMediaWikiApi($url);
+        $this->guzzleClient = $this->createGuzzleClient($identifier);
+        $this->api = new AddwikiMediaWikiApi($url, $this->guzzleClient);
+
+        /** @var CookieJar $jar */
+        $jar = $this->guzzleClient->getConfig('cookies');
+        if ($jar->getCookieByName('mediawiki_session')) {
+            $this->hasExistingSession = true;
+        }
+    }
+
+    /**
+     * Create a Guzzle client with a cookie jar based on given wiki identifier
+     */
+    protected function createGuzzleClient(string $identifier): Client
+    {
+        $cookieJar = new FileCookieJar(storage_path('app/mw-cookies/' . $identifier . '.json'), true);
+        return new Client([
+            'cookies' => $cookieJar,
+            'headers' => [
+                'User-Agent' => 'UTRS 2, https://github.com/utrs2/utrs',
+            ]
+        ]);
     }
 
     public function getAddWikiMediaWikiApi(): AddwikiMediaWikiApi
@@ -43,9 +75,29 @@ class RealMediaWikiApi implements MediaWikiApi
             return;
         }
 
+        if ($this->hasExistingSession) {
+            try {
+                $userInfoResponse = $this->api->getRequest(new SimpleRequest('query', ['meta' => 'userinfo']));
+
+                // mediawiki assigns user ID 0 to logged-out editors, so we can use that to check if we are logged in
+                if ($userInfoResponse['query']['userinfo']['id'] > 0) {
+                    $this->loggedIn = true;
+                    return;
+                }
+            } catch (Throwable $ignored) {}
+
+            // looks like our session has expired, let's just kill it
+            /** @var CookieJar $jar */
+            $jar = $this->guzzleClient->getConfig('cookies');
+            $jar->clear();
+
+            $this->hasExistingSession = false;
+        }
+
         if (config('wikis.login.username') && config('wikis.login.password')) {
             $this->api->login(new ApiUser(config('wikis.login.username'), config('wikis.login.password')));
             $this->loggedIn = true;
+            $this->hasExistingSession = true;
             return;
         }
 
