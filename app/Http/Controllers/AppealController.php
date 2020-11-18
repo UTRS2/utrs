502a643cccea9440f067fed5abc6d0b76306b87f
Taavi Väänänen <hi@tassu.me>
2020-11-18 10:42:43 +0200

    Remove duplicate import

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -13,7 +13,6 @@ use App\Models\Sendresponse;
 use App\Models\Template;
 use App\Models\User;
 use App\Services\Facades\MediaWikiRepository;
-use Auth;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
