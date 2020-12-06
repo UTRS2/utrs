5ef46008e5e0cecb21337722297421804cca3d4f
Taavi Väänänen <hi@tassu.me>
2020-12-06 15:54:38 +0200

    Add basic test for viewing templates

---
+++ tests/Feature/Admin/Templates/TemplateListTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Admin\Templates;
+
+use App\Models\Wiki;
+use App\Models\Template;
+use Tests\Traits\TestHasUsers;
+use Tests\Traits\SetupDatabaseForTesting;
+use Tests\TestCase;
+
+class TemplateListTest extends TestCase
+{
+    use TestHasUsers;
+    use SetupDatabaseForTesting;
+
+    public function test_tooladmin_can_view_template_list()
+    {
+        $user = $this->getTooladminUser();
+
+        // sanity check
+        $this->assertEquals(['enwiki'], $user->permissions->where('tooladmin', 1)->pluck('wiki')->all());
+
+        $wikiWithTooladmin = Wiki::where('database_name', 'enwiki')->first();
+        $wikiWithoutTooladmin = Wiki::where('database_name', '!=', 'enwiki')->first();
+
+        $templateCanSee = Template::factory()->create([ 'wiki_id' => $wikiWithTooladmin->id, ]);
+        $templateCantSee = Template::factory()->create([ 'wiki_id' => $wikiWithoutTooladmin->id, ]);
+
+        $response = $this
+            ->actingAs($user)
+            ->get('/admin/templates');
+
+        $response->assertSee($templateCanSee->name)
+            ->assertDontSee($templateCantSee->name);
+    }
+}
