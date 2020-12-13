35da3c33a488a6e4892ed2f1589d9d4485863a3f
Taavi Väänänen <hi@tassu.me>
2020-12-13 17:56:59 +0200

    Create useful seeds for development
    closes #288

---
+++ database/seeders/AppealSeed.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Appeal;
+use App\Models\Privatedata;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AppealSeed extends Seeder
+{
+    public function run()
+    {
+        $first = Appeal::factory()
+            ->create([ 'status' => Appeal::STATUS_DECLINE, ]);
+
+        Appeal::factory()
+            ->create([ 'appealfor' => $first->appealfor, ]);
+
+        Appeal::factory(5)
+            ->has(Privatedata::factory())
+            ->create();
+
+        $user = User::first();
+        if ($user) {
+            Appeal::factory(3)
+                ->has(Privatedata::factory())
+                ->create([ 'handlingadmin' => $user->id ]);
+        }
+
+        $otherUser = User::factory()->create();
+        Appeal::factory(3)
+            ->has(Privatedata::factory())
+            ->create([ 'handlingadmin' => $otherUser->id ]);
+    }
+}
