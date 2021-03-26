b80a187fef2a8f41f0dc4e7bddffcaad7a2c732f
Taavi Väänänen <hi@tassu.me>
2021-03-26 22:20:15 +0200

    Add new simpler logging API (#434)

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Utils\Logging\Loggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Template extends Model
 {
     use HasFactory;
+    use Loggable;
 
     protected $primaryKey = 'id';
     public $timestamps = false;
