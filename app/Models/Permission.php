9d7935c1332954b2c1347d39b79c99eaf3fdcf70
Taavi Väänänen <hi@tassu.me>
2021-12-07 15:51:25 +0200

    permission: Drop unused methods (#474)

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -61,23 +61,4 @@ class Permission extends Model
             ->intersect($perms)
             ->isNotEmpty();
     }
-
-    /** @deprecated use {@link Permission::hasAnySpecifiedPerms()} instead, or use policies */
-    public static function hasAnyPermission($userId, $wiki, $permissionArray)
-    {
-        abort_if(is_null($userId), 403, 'No logged in user');
-        return User::findOrFail($userId)->hasAnySpecifiedLocalOrGlobalPerms($wiki, $permissionArray);
-    }
-
-    /** @deprecated use {@link User::hasAnySpecifiedLocalOrGlobalPerms()} instead, or use policies */
-    public static function checkCheckuser($id, $wiki)
-    {
-        return self::hasAnyPermission($id, $wiki, ['checkuser']) || self::hasAnyPermission($id, '*', ['steward', 'staff', 'developer']);
-    }
-
-    /** @deprecated use {@link User::hasAnySpecifiedLocalOrGlobalPerms()} instead, or use policies */
-    public static function checkAdmin($id, $wiki)
-    {
-        return self::hasAnyPermission($id, $wiki, ['admin']) || self::hasAnyPermission($id, '*', ['steward', 'staff', 'developer']);
-    }
 }
