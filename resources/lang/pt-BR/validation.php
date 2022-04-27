a7ba3ac500d0ac9c876982b6043aa02e334ff27d
AmandaNP <deltaquadwiki@gmail.com>
2022-04-27 07:53:42 -0700

    Another update

--- resources/lang/pt-BR/validation.php
+++ resources/lang/pt-BR/validation.php
@@ -1,21 +1,21 @@
 <?php
 return [
-    'error-msg'            => 'ocorreram os seguintes erros:',
+    'error-msg'            => 'Ocorreram os seguintes erros:',
     'accepted'             => 'O :attribute deve ser aceito.',
     'active_url'           => 'O :attribute não é um URL válido.',
     'after'                => 'O :attribute deve ser uma data posterior a :date.',
     'after_or_equal'       => 'O :attribute deve ser uma data posterior ou igual a :date.',
-    'alpha'                => 'O :attribute só pode conter letras.',
+    'alpha'                => 'O :attribute pode conter apenas letras.',
     'alpha_dash'           => 'O :attribute pode conter apenas letras, números, traços e sublinhados.',
-    'alpha_num'            => 'O :attribute só pode conter letras e números.',
+    'alpha_num'            => 'O :attribute pode conter apenas letras e números.',
     'array'                => 'O :attribute deve ser uma matriz.',
     'before'               => 'O :attribute deve ser uma data anterior a :date.',
     'before_or_equal'      => 'O :attribute deve ser uma data anterior ou igual a :date.',
     'between'              => [
         'numeric' => 'O :attribute deve estar entre :min e :max.',
         'file'    => 'O :attribute deve estar entre :min e :max kilobytes.',
-        'string'  => 'O :attribute deve estar entre os caracteres :min e :max.',
-        'array'   => 'O :attribute deve ter itens entre :min e :max.'
+        'string'  => 'O :attribute deve estar entre :min e :max caracteres.',
+        'array'   => 'O :attribute deve ter entre :min e :max itens.'
     ],
     'boolean'              => 'O campo :attribute deve ser verdadeiro ou falso.',
     'confirmed'            => 'A confirmação :attribute não corresponde.',
@@ -23,8 +23,8 @@ return [
     'date_equals'          => 'O :attribute deve ser uma data igual a :date.',
     'date_format'          => 'O :attribute não corresponde ao formato :format.',
     'different'            => 'O :attribute e :other devem ser diferentes.',
-    'digits'               => 'O :attribute deve ser :digits dígitos.',
-    'digits_between'       => 'O :attribute deve estar entre os dígitos :min e :max.',
+    'digits'               => 'O :attribute deve ter :digits dígitos.',
+    'digits_between'       => 'O :attribute deve ter entre :min e :max dígitos.',
     'dimensions'           => 'O :attribute tem dimensões de imagem inválidas.',
     'distinct'             => 'O campo :attribute tem um valor duplicado.',
     'email'                => 'O :attribute deve ser um endereço de e-mail válido.',
@@ -41,8 +41,8 @@ return [
     'gte'                  => [
         'numeric' => 'O :attribute deve ser maior ou igual a :value.',
         'file'    => 'O :attribute deve ser maior ou igual a :value kilobytes.',
-        'string'  => 'O :attribute deve ser maior ou igual a :value caracteres.',
-        'array'   => 'O :attribute deve ter itens :value ou mais.'
+        'string'  => 'O :attribute deve ter :value ou mais caracteres.',
+        'array'   => 'O :attribute deve ter :value itens ou mais.'
     ],
     'image'                => 'O :attribute deve ser uma imagem.',
     'in'                   => 'O :attribute selecionado é inválido.',
@@ -87,15 +87,15 @@ return [
     'required_if'          => 'O campo :attribute é obrigatório quando :other é :value.',
     'required_unless'      => 'O campo :attribute é obrigatório, a menos que :other esteja em :values.',
     'required_with'        => 'O campo :attribute é obrigatório quando :values está presente.',
-    'required_with_all'    => 'O campo :attribute é obrigatório quando :values está presente.',
+    'required_with_all'    => 'O campo :attribute é obrigatório quando :values estão presentes.',
     'required_without'     => 'O campo :attribute é obrigatório quando :values não está presente.',
     'required_without_all' => 'O campo :attribute é obrigatório quando nenhum dos :values está presente.',
     'same'                 => 'O :attribute e :other devem corresponder.',
     'size'                 => [
         'numeric' => 'O :attribute deve ser :size.',
-        'file'    => 'O :attribute deve ser :size kilobytes.',
-        'string'  => 'O :attribute deve ter caracteres :size.',
-        'array'   => 'O :attribute deve conter itens :size.'
+        'file'    => 'O :attribute deve ter :size kilobytes.',
+        'string'  => 'O :attribute deve ter :size caracteres.',
+        'array'   => 'O :attribute deve conter :size itens.'
     ],
     'starts_with'          => 'O :attribute deve começar com um dos seguintes: :values',
     'string'               => 'O :attribute deve ser uma string.',
