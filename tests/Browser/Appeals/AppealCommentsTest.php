f9e9ab8791d91fe88e5192a7e36cd3725e197beb
AmandaNP <deltaquadwiki@gmail.com>
2022-04-30 11:59:20 -0700

    update tests

--- tests/Browser/Appeals/AppealCommentsTest.php
+++ tests/Browser/Appeals/AppealCommentsTest.php
@@ -51,8 +51,8 @@ class AppealCommentsTest extends DuskTestCase
         $this->browse(function (Browser $browser) use ($appeal, $targetTemplate, $targetTemplateTextStart, $nonActiveTemplate) {
             $browser->loginAs($this->getUser())
                 ->visit('/appeal/' . $appeal->id)
-                ->assertSee(Appeal::STATUS_OPEN)
-                ->assertDontSee(Appeal::STATUS_AWAITING_REPLY)
+                ->assertSee(__('appeals.status.OPEN'))
+                ->assertDontSee(__('appeals.status.AWAITING_REPLY'))
                 ->assertDontSee('Send a reply to user')
                 ->assertDontSee($targetTemplateTextStart)
                 ->press('Reserve')
@@ -65,8 +65,8 @@ class AppealCommentsTest extends DuskTestCase
                 ->assertSee($targetTemplateTextStart)
                 ->select('#status-' . $targetTemplate->id, Appeal::STATUS_AWAITING_REPLY)
                 ->press('Submit')
-                ->assertSee(Appeal::STATUS_AWAITING_REPLY)
-                ->assertDontSee(Appeal::STATUS_OPEN)
+                ->assertSee(__('appeals.status.AWAITING_REPLY'))
+                ->assertDontSee(__('appeals.status.OPEN'))
                 ->assertSee($targetTemplateTextStart);
         });
 
@@ -82,7 +82,7 @@ class AppealCommentsTest extends DuskTestCase
         $this->browse(function (Browser $browser) use ($appeal) {
             $browser->loginAs($this->getUser())
                 ->visit('/appeal/' . $appeal->id)
-                ->assertSee(Appeal::STATUS_OPEN)
+                ->assertSee(__('appeals.status.OPEN'))
                 ->assertDontSee('Send a reply to user')
                 ->press('Reserve')
                 ->clickLink('Send a reply to the user')
@@ -90,7 +90,7 @@ class AppealCommentsTest extends DuskTestCase
                 ->clickLink('Reply custom text')
                 ->type('custom', 'Go away.')
                 ->press('Submit')
-                ->assertSee(Appeal::STATUS_OPEN)
+                ->assertSee(__('appeals.status.OPEN'))
                 ->assertDontSee('set status as ');
         });
 
