49e19b22e7438e4ae216bf31994427a1b0c7eaf6
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2022-06-05 03:19:54 -0400

    add a badge for wiki listing (#564)

--- resources/views/components/appeal-table.blade.php
+++ resources/views/components/appeal-table.blade.php
@@ -19,26 +19,26 @@
             <tr>
         @endif
             <td>
-                <a href="{{ route('appeal.view', $appeal) }}" class="btn {{ $appeal->handlingadmin ? 'btn-danger' : 'btn-primary' }}">#{{ $appeal->id }}</a>
-                </td>
-                <td>{{ $appeal->appealfor }}</td>
-                <td style="vertical-align: middle">
-                    {{ __('appeals.status.'.$appeal->status) }}<br/>
-                    @if($appeal->blocktype === 0)
-                        {{__('appeals.appeal-types.ip')}}
-                    @elseif($appeal->blocktype === 1)
-                        {{__('appeals.appeal-types.account')}}
-                    @elseif($appeal->blocktype === 2)
-                        {{__('appeals.appeal-types.ip-under')}}
-                    @else
-                        {{__('appeals.appeal-types.unknown')}}: {{ $appeal->blocktype }}
-                    @endif
-                    {{__('generic.on')}} {{ $appeal->wiki }}
-                </td>
-                <td>{{ $appeal['blockingadmin'] }}</td>
-                <td>{!! $appeal->getFormattedBlockReason('style="color: #00ffea!important;"') !!}</td>
-                <td>{{ $appeal['submitted'] }}</td>
-            </tr>
+            <a href="{{ route('appeal.view', $appeal) }}" class="btn {{ $appeal->handlingadmin ? 'btn-danger' : 'btn-primary' }}">#{{ $appeal->id }}</a>
+            </td>
+            <td>{{ $appeal->appealfor }}</td>
+            <td style="vertical-align: middle">
+                {{ __('appeals.status.'.$appeal->status) }}<br/>
+                @if($appeal->blocktype === 0)
+                    {{__('appeals.appeal-types.ip')}}
+                @elseif($appeal->blocktype === 1)
+                    {{__('appeals.appeal-types.account')}}
+                @elseif($appeal->blocktype === 2)
+                    {{__('appeals.appeal-types.ip-under')}}
+                @else
+                    {{__('appeals.appeal-types.unknown')}}: {{ $appeal->blocktype }}
+                @endif
+                <br /><span class="badge bg-success" style="padding-right: 0.6em; padding-left: 0.6em; border-radius: 10rem;"><b>{{ $appeal->wiki }}</b></span>
+            </td>
+            <td>{{ $appeal['blockingadmin'] }}</td>
+            <td>{!! $appeal->getFormattedBlockReason('style="color: #00ffea!important;"') !!}</td>
+            <td>{{ $appeal['submitted'] }}</td>
+        </tr>
         @endforeach
     </tbody>
 </table>
