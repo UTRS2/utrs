e3336d2f332ef4664911efbbc5116af7cd48496e
AmandaNP <deltaquadwiki@gmail.com>
2022-06-10 16:00:28 -0700

    Add spanish

---
+++ resources/lang/es/home.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'noemail-header'      => 'MENSAJE IMPORTANTE',
+    'noemail-text'        => 'Durante este tiempo, no se enviarán correos electrónicos para apelaciones, excepto para bloqueos que no se encuentren para cuentas. Eso significa que debes tener a mano tu clave secreta de apelación en todo momento (NO LA COMPARTAS) y volver a consultarla periódicamente.',
+    'appeal-block-header' => 'Usuario bloqueado',
+    'appeal-block-title'  => 'Si tienes una cuenta de usuario',
+    'appeal-block-text'   => 'en Wikipedia y están bloqueados, selecciona el botón a continuación para iniciar tu apelación.',
+    'appeal-block-button' => 'Apelar mi bloqueo',
+    'appeal-ip-header'    => 'IP bloqueada',
+    'appeal-ip-title'     => 'Si NO tienes una cuenta de usuario',
+    'appeal-ip-text'      => 'en Wikipedia y están bloqueados, selecciona el botón a continuación para iniciar tu apelación.',
+    'appeal-ip-button'    => 'Apelar mi bloqueo de IP',
+    'admin-header'        => 'Iniciar sesión',
+    'admin-title'         => 'Para los administradores que están aquí',
+    'admin-text'          => 'para comentar, procesar o ayudar con las apelaciones, selecciona el botón a continuación. Ten en cuenta que solo los administradores pueden participar en este formulario.',
+    'admin-button'        => 'Ir a Apelaciones',
+    'login-button'        => 'Accede con tu cuenta de Wikimedia'
+];
---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent'  => '¡Te hemos enviado por correo electrónico tu enlace de restablecimiento de contraseña!',
+    'token' => 'Este token de restablecimiento de contraseña no es válido.',
+    'user'  => 'No podemos encontrar un usuario con esa dirección de correo electrónico.'
+];
