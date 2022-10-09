d7b91f1656687fcd4f17a3e407803191ef68bf6b
Taavi Väänänen <hi@taavi.wtf>
2022-10-09 20:15:46 +0300

    replace removed card-deck

--- resources/views/appeals/public/needauth.blade.php
+++ resources/views/appeals/public/needauth.blade.php
@@ -7,38 +7,42 @@
             {{ __('auth.auth-needed-screen.title') }}
         </h2>
 
-        <p>
+        <p class="mb-0">
             {{ __('auth.auth-needed-screen.text') }}
         </p>
     </div>
 
-    <div class="card-deck">
-        <div class="card">
-            <div class="card-body">
-                {{ __('auth.auth-needed-screen.key-text') }}
+    <div class="row row-cols-2">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    {{ __('auth.auth-needed-screen.key-text') }}
 
-                {{ Form::open(['url' => route('public.appeal.view'), 'method' => 'GET']) }}
-                <div class="input-group w-100 mb-3">
-                    {{ Form::text('hash', null, ['class' => 'form-control w-100','placeholder' => __('auth.auth-needed-screen.key-placeholder')]) }}
-                </div>
+                    {{ Form::open(['url' => route('public.appeal.view'), 'method' => 'GET']) }}
+                    <div class="input-group w-100 mb-3">
+                        {{ Form::text('hash', null, ['class' => 'form-control w-100','placeholder' => __('auth.auth-needed-screen.key-placeholder')]) }}
+                    </div>
 
-                <div>
-                    <button type="submit" class="btn btn-primary">{{ __('auth.auth-needed-screen.submit-text') }}</button>
-                    {{-- <a href="#" class="btn btn-danger">Forgot Appeal Key</a> --}}
+                    <div>
+                        <button type="submit" class="btn btn-primary">{{ __('auth.auth-needed-screen.submit-text') }}</button>
+                        {{-- <a href="#" class="btn btn-danger">Forgot Appeal Key</a> --}}
+                    </div>
+                    {{ Form::close() }}
                 </div>
-                {{ Form::close() }}
             </div>
         </div>
 
-        <div class="card">
-            <div class="card-body">
-                <p>
-                    {{ __('auth.auth-needed-screen.login-text') }}
-                </p>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <p>
+                        {{ __('auth.auth-needed-screen.login-text') }}
+                    </p>
 
-                <a href="?send_to_oauth=1" class="btn btn-info">
-                    {{ __('auth.auth-needed-screen.oauth-button') }}
-                </a>
+                    <a href="?send_to_oauth=1" class="btn btn-info">
+                        {{ __('auth.auth-needed-screen.oauth-button') }}
+                    </a>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/appeals/public/wrongkey.blade.php
+++ resources/views/appeals/public/wrongkey.blade.php
@@ -7,27 +7,25 @@
             {{ __('appeals.wrong-key.title') }}
         </h2>
 
-        <p>
+        <p class="mb-0">
             {{ __('appeals.wrong-key.text') }}
         </p>
     </div>
 
-    <div class="card-deck">
-        <div class="card">
-            <div class="card-body">
-                {{ __('auth.auth-needed-screen.key-text') }}
+    <div class="card">
+        <div class="card-body">
+            {{ __('auth.auth-needed-screen.key-text') }}
 
-                {{ Form::open(['url' => route('public.appeal.view'), 'method' => 'GET']) }}
-                <div class="input-group w-100 mb-3">
-                    {{ Form::text('hash', null, ['class' => 'form-control w-100','placeholder' => __('auth.auth-needed-screen.key-placeholder')]) }}
-                </div>
-
-                <div>
-                    <button type="submit" class="btn btn-primary">{{ __('auth.auth-needed-screen.submit-text') }}</button>
-                    {{-- <a href="#" class="btn btn-danger">Forgot Appeal Key</a> --}}
-                </div>
-                {{ Form::close() }}
+            {{ Form::open(['url' => route('public.appeal.view'), 'method' => 'GET']) }}
+            <div class="input-group w-100 mb-3">
+                {{ Form::text('hash', null, ['class' => 'form-control w-100','placeholder' => __('auth.auth-needed-screen.key-placeholder')]) }}
             </div>
+
+            <div>
+                <button type="submit" class="btn btn-primary">{{ __('auth.auth-needed-screen.submit-text') }}</button>
+                {{-- <a href="#" class="btn btn-danger">Forgot Appeal Key</a> --}}
+            </div>
+            {{ Form::close() }}
         </div>
     </div>
 @endsection
