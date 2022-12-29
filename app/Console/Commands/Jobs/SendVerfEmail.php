782c96e40ae1d12586440d2806c441a1f25db45c
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2022-12-28 23:37:23 -0500

    Add cmd to send email to an appeal already on file

---
+++ app/Console/Commands/Jobs/SendVerfEmail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands\Jobs;
+
+use Illuminate\Console\Command;
+use App\Services\Facades\MediaWikiRepository;
+use App\Jobs\Scheduled\RemoveAppealPrivateDataJob;
+use App\Models\Appeal;
+use App\Services\Facades\MediaWikiRepository;
+
+class RemoveAppealPrivateDataCommand extends Command
+{
+    protected $signature = 'utrs-jobs:verf-email {id}';
+    protected $description = 'Send Verification email';
+
+    public function handle()
+    {
+        if (!is_numeric($id)) {
+          $this->error('Error: ID is not numeric')
+          return 1;
+        }
+        $this->info('Sending Verf Email...');
+        $appeal = Appeal::findOrFail($id);
+        $url = url(route('public.appeal.verifyownership', [$appeal, $token]));
+        $title = 'UTRS appeal verification';
+         $message = <<<EOF
+    Hello,
+    Someone appealed your Wikipedia block using the Unblock Ticket Request System (UTRS).
+    If this was you, please verify this appeal by using this link:
+    $url
+    If this wasn't you, no action is needed.
+    Thanks,
+    the UTRS team
+    EOF;
+        
+        $result = MediaWikiRepository::getApiForTarget($appeal->wiki)->getMediaWikiExtras()->sendEmail($appeal->getWikiEmailUsername(), $title, $message);
+        $this->info('Result: '.$result)
+        $this->info('Done sending');
+        return 0;
+    }
+}
