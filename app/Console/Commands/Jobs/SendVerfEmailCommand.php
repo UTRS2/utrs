53a4c5f05d9e0cdf20d19d4983821ea87897132e
Amanda/DeltaQuad <deltaquadwiki@gmail.com>
2023-01-20 01:12:08 -0500

    It all works, I was just being silly

--- app/Console/Commands/Jobs/SendVerfEmailCommand.php
+++ app/Console/Commands/Jobs/SendVerfEmailCommand.php
@@ -28,7 +28,6 @@ class SendVerfEmailCommand extends Command
             $this->info("User hasn't set email address onwiki");
             $appeal->user_verified=-1;
             $appeal->save();
-            dd($appeal);
             return;
         }
         $title = 'UTRS appeal verification';
