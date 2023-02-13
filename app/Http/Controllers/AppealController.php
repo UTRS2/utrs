cc14d0632ca6786c14723cff530a6d4525347b05
AmandaNP <deltaquadwiki@gmail.com>
2023-02-13 08:37:28 -0500

    Add response during custom reply and fix url links

--- app/Http/Controllers/AppealController.php
+++ app/Http/Controllers/AppealController.php
@@ -271,7 +271,7 @@ class AppealController extends Controller
         
         if ($appeal->user_verified==1) {
             $title = 'UTRS appeal response';
-            $baseURL = route('public.appeal.view');
+            $baseURL = route('home');
             $message = <<<EOF
                 Hello,
                 Your appeal, #$appeal->id, has be reviewed and the following message was left for you:
@@ -331,6 +331,24 @@ class AppealController extends Controller
             'ua' => $ua . " " . $lang,
             'protected' => LogEntry::LOG_PROTECTION_NONE,
         ]);
+        
+        if ($appeal->user_verified==1) {
+            $title = 'UTRS appeal response';
+            $baseURL = route('home');
+            $message = <<<EOF
+                Hello,
+                Your appeal, #$appeal->id, has be reviewed and the following message was left for you:
+
+                $request->input('custom')
+
+                Please reply by going to the following link and entering your appealkey: $baseURL
+                In case you forgot your appealkey, it is: $appeal->appealsecretkey
+
+                Thanks,
+                the UTRS team
+                EOF;
+            $result = MediaWikiRepository::getApiForTarget($appeal->wiki)->getMediaWikiExtras()->sendEmail($appeal->getWikiEmailUsername(), $title, $message);
+        }
 
         return redirect()->route('appeal.view', $appeal);
     }
