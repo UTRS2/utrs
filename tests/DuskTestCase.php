fee6856fe19909298396de6fcdc98e14ccd487b7
AmandaNP <deltaquadwiki@gmail.com>
2023-05-21 16:14:49 -0400

    unnecessary

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -34,7 +34,6 @@ abstract class DuskTestCase extends BaseTestCase
             '--disable-gpu',
             '--headless',
             '--window-size=1920,1080',
-            '--lang=en_US',
         ]);
 
         return RemoteWebDriver::create(
