9475d29afe34125233a108f3006653e4d0a0de2f
AmandaNP <deltaquadwiki@gmail.com>
2023-05-21 14:51:53 -0400

    It's a chromedriver arg

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -34,6 +34,7 @@ abstract class DuskTestCase extends BaseTestCase
             '--disable-gpu',
             '--headless',
             '--window-size=1920,1080',
+            '--lang=en_US',
         ]);
 
         return RemoteWebDriver::create(
