eb79c41f6e5d2f96a6cdf4d43fbe96aa1823ca4a
AmandaNP <deltaquadwiki@gmail.com>
2023-07-23 21:51:26 -0400

    Add client hint support for appeals #259

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \App\Http\Middleware\AddHeaders::class,
 
         ForceHttps::class,
         AddHstsHeader::class,
