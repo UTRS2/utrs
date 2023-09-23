62b48f8f5f666634963333ac96b32ee52cd01822
AmandaNP <deltaquadwiki@gmail.com>
2023-09-23 14:30:02 -0400

    expand to include global

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -16,24 +16,44 @@ class StatsController extends Controller
 
     public function display_appeals_chart()
     {
-        $results = Appeal::whereTime('submitted', '>',Carbon::now()->subDays(90))->get();
-        $data = \Lava::DataTable();
-        $data = $data->addStringColumn('appstat')
+        $enwiki = Appeal::whereTime('submitted', '>',Carbon::now()->subDays(90))->where('wiki_id',1)->get();
+        
+        $en_data = \Lava::DataTable();
+        $en_data = $en_data->addStringColumn('enwiki_appstat')
             ->addNumberColumn('Number of appeals')
-            ->addRow(['Total appeals in time period', $results->count()])
-            ->addRow(['Accepted', $results->where('status', Appeal::STATUS_ACCEPT)->count()])
-            ->addRow(['Declined', $results->where('status', Appeal::STATUS_DECLINE)->count()])
-            ->addRow(['Expired', $results->where('status', Appeal::STATUS_EXPIRE)->count()]);
+            ->addRow(['Total appeals in time period', $enwiki->count()])
+            ->addRow(['Accepted', $enwiki->where('status', Appeal::STATUS_ACCEPT)->count()])
+            ->addRow(['Declined', $enwiki->where('status', Appeal::STATUS_DECLINE)->count()])
+            ->addRow(['Expired', $enwiki->where('status', Appeal::STATUS_EXPIRE)->count()]);
 
-        \Lava::BarChart('appstat', $data, [
-            'title' => 'Appeals in the last 90 days',
+        \Lava::BarChart('enwiki_appstat', $en_data, [
+            'title' => 'Appeals in the last 90 days - enwiki',
             'legend' => [
                 'position' => 'none'
             ],
-            'colors' => ['#000000', '#00FF00', '#FF0000', '#0000FF'],
+            'colors' => ['#00FF00', '#FF0000', '#0000FF'],
             'height' => 500,
             'width' => 1000,
         ]);
-        return view('stats.appeals', ['chart' => $data]);
+
+        $global = Appeal::whereTime('submitted', '>',Carbon::now()->subDays(90))->where('wiki_id',3)->get();
+        $g_data = \Lava::DataTable();
+        $g_data = $g_data->addStringColumn('global_appstat')
+            ->addNumberColumn('Number of appeals')
+            ->addRow(['Total appeals in time period', $global->count()])
+            ->addRow(['Accepted', $global->where('status', Appeal::STATUS_ACCEPT)->count()])
+            ->addRow(['Declined', $global->where('status', Appeal::STATUS_DECLINE)->count()])
+            ->addRow(['Expired', $global->where('status', Appeal::STATUS_EXPIRE)->count()]);
+
+        \Lava::BarChart('global_appstat', $g_data, [
+            'title' => 'Appeals in the last 90 days - Global',
+            'legend' => [
+                'position' => 'none'
+            ],
+            'colors' => ['#FF0000', '#0000FF'],
+            'height' => 500,
+            'width' => 1000,
+        ]);
+        return view('stats.appeals');
     }
 }
--- resources/views/stats/appeals.blade.php
+++ resources/views/stats/appeals.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app')
 @section('title', 'Statistics - Appeals')
 @section('content')
-<div id="chart_div"></div>
-@barchart('appstat', 'chart_div')
+<div id="en_div"></div>
+<div id="meta_div"></div>
+@barchart('enwiki_appstat', 'en_div')
+@barchart('global_appstat', 'meta_div')
 @endsection
