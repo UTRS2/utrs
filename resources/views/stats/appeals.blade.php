c3c43394dbccadfa5a0e771044b28357e85672af
AmandaNP <deltaquadwiki@gmail.com>
2023-09-23 19:20:17 -0400

    forgot to pull this

--- resources/views/stats/appeals.blade.php
+++ resources/views/stats/appeals.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Statistics - Appeals')
 @section('content')
-@foreach($dates as $date)
-    {{$date}}<br />
-@endforeach
 <div id="enwiki_day_div"></div>
 <div id="global_day_div"></div>
 <div id="en_div"></div>
