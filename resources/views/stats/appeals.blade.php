a3705123d427e8a9d446c46217936df9df8bf98d
AmandaNP <deltaquadwiki@gmail.com>
2023-09-23 21:02:18 -0400

    debug

--- resources/views/stats/appeals.blade.php
+++ resources/views/stats/appeals.blade.php
@@ -7,8 +7,10 @@
 <div id="meta_div"></div>
 <div id="en_blockadmin_div"></div>
 <div id="en_blockreason_div"></div>
-Number of appeals with other reasons: {{ $other }}
-Reasons: {{ $reasons }}
+Number of appeals with other reasons: {{ $other }}<br />
+@foreach($reasons as $reason)
+{{ $reason->reason }}: {{ $reason->count }}<br />
+@endforeach
 @columnchart('enwiki_daystat', 'enwiki_day_div')
 @columnchart('global_daystat', 'global_day_div')
 @barchart('enwiki_appstat', 'en_div')
