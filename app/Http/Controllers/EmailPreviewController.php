cf8e676a5414faf6003db80c75fc8f0856dd5ad2
AmandaNP <deltaquadwiki@gmail.com>
2024-04-14 13:23:40 -0400

    it was right to start

--- app/Http/Controllers/EmailPreviewController.php
+++ app/Http/Controllers/EmailPreviewController.php
@@ -21,10 +21,9 @@ class EmailPreviewController extends Controller
         return view('admin.emailspreview', ['emails' => $emails]);
     }
 
-    public function previewEmail($email, Request $request) {
-        // if the xff ip address from the header is within RFC1918
-        dd($request->ip());
-        if (!filter_var(request()->server('HTTP_X_FORWARDED_FOR'), FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
+    public function previewEmail($email) {
+        // if the ip address from the header is within RFC1918
+        if (!filter_var(request()->ip(), FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
             // if user is not a developer, return 403
             abort_unless(Auth::check(), 403, 'No logged in user');
             abort_unless(Auth::user()->hasAnySpecifiedLocalOrGlobalPerms([], 'developer'), 403, 'You are not a developer');
