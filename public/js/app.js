c81eaddde65aece43eedf8e29ba93198bd196345
AmandaNP <deltaquadwiki@gmail.com>
2026-03-16 10:34:02 -0400

    Remove Previously commented out popper

--- public/js/app.js
+++ public/js/app.js
@@ -3017,7 +3017,6 @@ function withinMaxClamp(min, value, max) {
   \*****************************/
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
 
-import Popper from 'popper.js/dist/umd/popper.js';
 window.$ = window.jQuery = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,2 @@
-import Popper from 'popper.js/dist/umd/popper.js';
 window.$ = window.jQuery = require('jquery');
 import 'bootstrap';
